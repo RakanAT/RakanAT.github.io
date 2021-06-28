0b9cbba03ebb0c620953ccdb8d137531891aaf10
RakanAT <47198475+RakanAT@users.noreply.github.com>
2021-06-27 23:12:10 -0400

    more last minute changes

--- Module6-EnhancedHealthSite/scripts/form.js
+++ Module6-EnhancedHealthSite/scripts/form.js
@@ -1,17 +1,97 @@
-const phone = document.getElementById('phone')
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const credit = document.getElementById('credit')
-const cvc = document.getElementById('cvc')
-const billing = document.getElementById('billing')
+function checkCredit(creditInputID) {
+  var a = document.getElementById(creditInputID).value;
+  var filter = new RegExp(/^([0-9]{4}\s){3}[0-9]{4}$/);
+  // return filter.test(a);
+  console.log(filter.test(a));
+  console.log(typeof a);
 
-billing.addEventListener('submit', (e)=>{
-	let messages = []
-		if (name.value === '' || name.value == null){
-			messages.push('Name is Required')
-		}
-		if (messages.length > 0) {
-			e.preventDefault()
-			errorElement.innerText = messages.join(',')
-		}
+  return filter.test(a);
 }
+
+function checkPhone(phoneInputID){
+    var a = document.getElementById(phoneInputID).value;
+    var filter = new RegExp(/\([0-9]{3}\)\s([0-9]{3})\-([0-9]{4})/);
+    return filter.test(a);
+}
+var unavailableDates = ["06/29/2020", "07/07/2020", "07/10/2020"];
+const setDateFormat = "mm/dd/yy";
+
+function disableDates(date,VetSelectid) {
+  // Sunday is Day 0, disable all Sundays
+  
+  var doctor = document.getElementById("doctor-select").value;
+  if(doctor == "Dr. Ismail"){
+    if (date.getDay() === 0) return [false];
+  }else if(doctor == "Dr. Abdo"){
+    if (date.getDay() === 1) return [false];
+  }else if(doctor == "Dr. Rakan"){
+    if (date.getDay() === 4) return [false];
+  }
+  var string = jQuery.datepicker.formatDate(setDateFormat, date);
+  return [unavailableDates.indexOf(string) === -1];
+}
+
+$(document).ready(function () {
+  $("#PetType-text-input").tooltip({
+    classes: {
+      "ui-tooltip": "highlight",
+    },
+  });
+  $("#Credit-text-input").tooltip({
+    classes: {
+      "ui-tooltip": "highlight",
+    },
+  });
+  $("#phone-text-input").on("change", function () {
+    if (!checkPhone("phone-text-input")) {
+      alert("Wrong format for phone number");
+      $("#phone-text-input").val("(xxx) xxx-xxxx");
+      $("#phone-text-input").addClass("error");
+    } else {
+      $("#phone-text-input").removeClass("error");
+    }
+  });
+  $("#Credit-text-input").on("change", function () {
+    if (!checkCredit("Credit-text-input")) {
+      alert("Wrong format for credit card");
+      $("#Credit-text-input").val("xxxx xxxx xxxx xxxx");
+      $("#Credit-text-input").addClass("error");
+    } else {
+      $("#Credit-text-input").removeClass("error");
+    }
+  });
+  $("#dateInput").datepicker({
+    dateFormat: setDateFormat,
+    // no calendar before June 1rst 2020
+    minDate: new Date("06/01/2020"),
+    maxDate: "+4M",
+    // used to disable some dates
+    // beforeShowDay: $.datepicker.noWeekends,
+    beforeShowDay: disableDates
+  });
+  $("#btnSaveChange").click(function (e) {
+    e.preventDefault();
+    var name = $("#fullname-text-input").val();
+  
+    var date = $("#dateInput").val();
+    var email = $("#email-text-input").val();
+    var service = $("#service-select").val();
+    var doctor = $("#doctor-select").val();
+    alert(
+      "Thank You " +
+        name +
+        "(" +
+        email +
+        ")" +
+        "," +
+        " will be in a " +
+        service +
+        " appointment on " +
+        date + 
+        " with " +
+        doctor + "."
+    );
+
+    $("#AppointmentModal").modal("toggle");
+  });
+});
